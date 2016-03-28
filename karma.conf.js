4fd148d1b27dda0667cba2f843e0f8a91a6fbd05
Chiehlun <wuceh14678@gmail.com>
2016-03-28 10:29:44 +0800

    try to add TravisCI

--- karma.conf.js
+++ karma.conf.js
@@ -2,82 +2,96 @@
 // Generated on Sun Mar 27 2016 16:53:43 GMT+0800 (CST)
 var webpackConfig = require('./webpack.config.test')
 
+var configuration = {
+  // base path that will be used to resolve all patterns (eg. files, exclude)
+  basePath: 'src',
+
+
+  // frameworks to use
+  // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
+  frameworks: ['jasmine'],
+
+
+  // list of files / patterns to load in the browser
+  files: [
+    '**/*.spec.js',
+  ],
+
+
+  // list of files to exclude
+  // exclude: [
+  //   'client.js'
+  // ],
+
+
+  // preprocess matching files before serving them to the browser
+  // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+  preprocessors: {
+    '**/*.spec.js': ['webpack']
+  },
+
+  webpack:  webpackConfig,
+
+
+  //Don't spam the console when running in karma!
+  webpackServer: {
+    noInfo: true
+  },
+
+
+  // test results reporter to use
+  // possible values: 'dots', 'progress'
+  // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+  reporters: ['spec', 'coverage'],
+  coverageReporter: {
+    type: 'text'
+  },
+
+
+  // web server port
+  port: 9876,
+
+
+  // enable / disable colors in the output (reporters and logs)
+  colors: true,
+
+  // enable / disable watching file and executing tests whenever any file changes
+  autoWatch: false,
+
+
+  // start these browsers
+  // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+  browsers: ['Chrome'],
+
+
+
+  customLaunchers: {
+    Chrome_travis_ci: {
+        base: 'Chrome',
+        flags: ['--no-sandbox']
+    }
+  },
+
+  // Continuous Integration mode
+  // if true, Karma captures browsers, runs the tests and exits
+  singleRun: true,
+
+  // Concurrency level
+  // how many browser should be started simultaneous
+  concurrency: Infinity
+}
+
+
+
 module.exports = function(config) {
-  config.set({
 
-    // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: 'src',
+  if(process.env.TRAVIS){
+    configuration.browsers = ['Chrome_travis_ci'];
+  }
 
+  // level of logging
+  // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+  configuration.logLevel = config.LOG_INFO
 
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      '**/*.spec.js',
-    ],
-
-
-    // list of files to exclude
-    // exclude: [
-    //   'client.js'
-    // ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {
-      '**/*.spec.js': ['webpack']
-    },
-
-    webpack:  webpackConfig,
-
-
-    //Don't spam the console when running in karma!
-    webpackServer: {
-      noInfo: true
-    },
-
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['spec', 'coverage'],
-    coverageReporter: {
-      type: 'text'
-    },
-
-
-    // web server port
-    port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
-    colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
-    logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
-
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
-    concurrency: Infinity
-  })
+  config.set(configuration)
 }
